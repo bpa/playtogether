3a3aae711678f76d1c74e2f382b4f7bc0521d142
Bruce Armstrong <bruce@fortressofgeekdom.org>
2012-10-31 16:29:45 -0600

    Made test fail properly

--- lib/Gamed/Network/INET.pm
+++ lib/Gamed/Network/INET.pm
@@ -1,12 +1,21 @@
 module Gamed::Network;
 
 class INET {
-	has IO::Socket $!sock;
+	has IO::Socket $sock;
+	has IO::Socket @clients;
 	
 	submethod new (Int $port) {
 		self.bless(*, sock => IO::Socket::INET.new(:localhost('0.0.0.0'), :localport($port), :listen));
 	}
 
 	method poll() {
+		if $sock.poll(1,0) {
+			@clients.push($sock.accept());
+		}
+		for @clients -> $c {
+			if $c.poll(1,0) {
+				$c.write($c.read());
+			}
+		}
 	}
 }
