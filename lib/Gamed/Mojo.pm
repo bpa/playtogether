c29a45ec4ec43dc5439db79f029826242c9fcf8a
Bruce Armstrong <bruce@fortressofgeekdom.org>
2013-04-02 16:38:41 -0600

    Starting integration rework

--- lib/Gamed/Mojo.pm
+++ lib/Gamed/Mojo.pm
@@ -22,65 +22,67 @@ $sessions->default_expiration(86400);
 get '/' => sub {
     my $self = shift;
     my $user = $self->session('user');
-    my $game = $self->session('game');
-    $self->app->log->debug("User: $user");
-    $self->render(
-        defined $user ? 'index' : 'login',
-        user => $user,
-        game => $game
-    );
-};
-
-get qr!/game/:game! => sub {
-	
-};
-
-post '/login' => sub {
-    my $self = shift;
-    my $user = $self->param('username');
     if ( defined $user ) {
-        $self->session( user => $user );
-        $self->session( game => 'lobby' );
-        $self->render( 'index', user => $user, game => 'lobby' );
+ 		$self->render('lobby', user => $user, game => 'Lobby', games => [ sort keys(%Gamed::games) ] );
     }
     else {
         $self->render('login');
     }
 };
 
-get '/lobby' => sub {
+post '/' => sub {
     my $self = shift;
-    $self->render('lobby', games => [sort keys(%Gamed::games)]);
+    my $user = $self->param('username');
+    if ( defined $user ) {
+		$self->session('user', $user);
+		return $self->redirect_to('/');
+    }
+    else {
+        $self->render('login');
+    }
 };
 
-websocket '/websocket' => sub {
+get '/game/:name' => sub {
+};
+
+get '/game/:game/create' => sub {
+	my $self = shift;
+	my $game = $self->param('game');
+	if (exists $Gamed::games{$game}) {
+		
+	}
+	else {
+		$self->stash(error => "No game named '$game' exists");
+		$self->redirect_to('/');
+	}
+};
+
+websocket '/game/:name/websocket' => sub {
     my $self = shift;
     $self->app->log->debug('WebSocket connected.');
     Mojo::IOLoop->stream( $self->tx->connection )->timeout(3600);
-	my $user = $self->session('user');
-	my $id = Gamed::on_connect($user, $self);
-	$self->app->log->debug($self);
+    my $user = $self->session('user');
+    my $id = Gamed::on_connect( $user, $self );
+    $self->app->log->debug($self);
 
     $self->on(
         message => sub {
             my ( $self, $msg ) = @_;
-			$self->app->log->debug($msg);
-			Gamed::on_message($id, $msg);
-			$self->app->log->debug($self);
-        }
-    );
+            $self->app->log->debug($msg);
+            Gamed::on_message( $id, $msg );
+            $self->app->log->debug($self);
+        } );
 
     $self->on(
         finish => sub {
             my $self = shift;
             $self->app->log->debug('WebSocket disconnected.');
-			Gamed::on_disconnect($user);
-        }
-    );
+            Gamed::on_disconnect($user);
+        } );
 };
 
 my $daemon = Mojo::Server::Daemon->new( app => app, listen => ['http://*:8080'] );
-$daemon->app->home->parse( catdir( dirname(__FILE__), '..', 'Gamed' ), 'Gamed');
+$daemon->app->home->parse( catdir( dirname(__FILE__), '..', 'Gamed' ), 'Gamed' );
 $daemon->app->static->paths->[0]   = $daemon->app->home->rel_dir('public');
 $daemon->app->renderer->paths->[0] = $daemon->app->home->rel_dir('templates');
 $daemon->start;
