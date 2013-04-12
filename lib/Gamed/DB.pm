70fa6024a129ab6e81ff804e5175d4d882307aa3
Bruce Armstrong <bruce@fortressofgeekdom.org>
2013-04-12 16:51:37 -0600

    Switch div when entering bidding state

--- lib/Gamed/DB.pm
+++ lib/Gamed/DB.pm
@@ -6,12 +6,12 @@ use Authen::Passphrase::SaltedDigest;
 my $uuid = Data::UUID->new;
 my $dbh = DBI->connect("dbi:SQLite:data") or die "Could not connect to database";
 unless ( $dbh->selectrow_arrayref("SELECT 1 FROM sqlite_master WHERE type='table' AND name = 'user'") ) {
-    $dbh->do("create table user (username text PRIMARY KEY, passphrase, name text, avatar text)");
+    $dbh->do("create table users (username text PRIMARY KEY, passphrase, name text, avatar text)");
 }
 
 helper login => sub {
     my $self = shift;
-    my $user = $dbh->selectall_arrayref( "select * from user where username=?", { Slice => {} }, $self->param('username'), );
+    my $user = $dbh->selectall_arrayref( "select * from users where username=?", { Slice => {} }, $self->param('username'), );
     return unless @$user > 0;
     $user = $user->[0];
     my $ppr = Authen::Passphrase::SaltedDigest->from_rfc2307( delete $user->{passphrase} );
@@ -26,7 +26,7 @@ helper create_user => sub {
         passphrase  => $self->param('passphrase') );
     if (
         $dbh->do(
-            "insert into user (username, passphrase, name, avatar) values (?,?,?,?)",
+            "insert into users (username, passphrase, name, avatar) values (?,?,?,?)",
             {}, $self->param('username'),
             $ppr->as_rfc2307, $self->param('name'), $self->param('avatar') ) )
     {
